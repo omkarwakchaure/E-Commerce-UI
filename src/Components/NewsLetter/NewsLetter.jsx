d69a1a9ac4ee2bbc2f959d9be5eb5c508e2be68c
Omkar Wakchaure <omkarwakchaure2019@gmail.com>
2024-01-19 01:30:29 +0530

    css changes

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -4,7 +4,7 @@ const NewsLetter = () => {
 return(
 <div className='newsletter'>
 <h1>Get Exclusive Offers On Your Email</h1>
-<p>Subscribe to our newletter and stay updated</p>
+<p>Subscribe to our newsletter and stay updated</p>
 <div>
 <input type="email" placeholder='Your Email id' />
 <button>Subscribe</button>
